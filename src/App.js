c2cf69d8237abce9c40d815876570eb3c2281860
Horev Vladimir <vshorev@yandex.ru>
2025-04-03 21:58:24 +0700

    тестовое задание на фронта2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+  const [myId, setMyId] = useState(null);
+  const [filtered, setFiltered] = useState([]);
+
+  // делаем запрос на api, получаем данные
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts([posts, ...data]))
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(response => response.json())
+      .then(data => setComments([comments, ...data]))
+  }, [])
+
+  // функция которая фильтрует по айти данный с стейта комментарий по айди
+  const getPostId = (id) => {
+    // проверяем что id пробрасывается правильно
+    setMyId(id)
+    let result = comments.filter(elem => elem.postId == id);
+    setFiltered([filtered, ...result])
+
+    console.log(result)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex_position'>
+
+      <div>
+        <p className='text_position'>Получаем спискок наших сообщений</p>
+        <ul>
+          {/* мапаем наш  массив с постами */}
+          {posts.map(post => (
+            <li key={post.id} onClick={() => getPostId(post.id)}   >
+              {post.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        {/* мапаем фильтрованный массив */}
+        <p>Получаем список наших  комментарий</p>
+        {filtered.map(item => (
+          <li key={item.id}    >
+            {item.body}
+          </li>
+        ))}
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/components/Header_component.js
@@ -0,0 +1,13 @@
+import '../App.css';
+
+function App() {
+  return (
+    <div >
+      <header className='my_flex' >
+        Тестовое задание Хорев В.С.
+      </header>
+    </div>
+  );
+}
+
+export default App;
